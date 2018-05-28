558ac4deea30908c11ae6ea09035c9fca99abaa0
jaxx2104 <jaxx2104@gmail.com>
2018-05-29 00:25:25 +0900

    Fix node_modules

--- components/Auth/Auth.story.js
+++ components/Auth/Auth.story.js
@@ -1,11 +1,9 @@
 import { storiesOf } from '@storybook/vue'
-import VueInfoAddon from 'storybook-addon-vue-info'
 import Auth from './Auth.vue'
 import store from '~/store'
 import '~/assets/css/storybook.css'
 
 storiesOf('Auth', module)
-  .addDecorator(VueInfoAddon)
   .add('default', () => ({
     components: { Auth },
     store,
--- components/Button/Button.story.js
+++ components/Button/Button.story.js
@@ -1,10 +1,8 @@
 import { storiesOf } from '@storybook/vue'
-import VueInfoAddon from 'storybook-addon-vue-info'
 import Button from './Button.vue'
 import '~/assets/css/storybook.css'
 
 storiesOf('Button', module)
-  .addDecorator(VueInfoAddon)
   .add('default', () => ({
     components: { Button },
     template: `<Button prefix="👍"/>`
