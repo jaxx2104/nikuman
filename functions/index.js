b577862e0ebd12d6ee0fbbd0280f5e403e9d7367
jaxx2104 <jaxx2104@gmail.com>
2018-08-16 14:30:04 +0900

    Add express cache

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,18 @@
+const express = require('express')
 const functions = require('firebase-functions')
 const { Nuxt } = require('nuxt')
 
+const app = express()
 const nuxt = new Nuxt({ buildDir: 'ssr', dev: false })
 
-exports.ssr = functions.https.onRequest(nuxt.render)
+const handleRequest = (req, res) => {
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, promise => {
+      promise.then(resolve).catch(reject)
+    })
+  })
+}
+
+app.use(handleRequest)
+exports.ssr = functions.https.onRequest(app)
