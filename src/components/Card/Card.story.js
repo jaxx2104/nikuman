b33dada87d635f12502f317e07854bfded9f2b38
jaxx2104 <jaxx2104@gmail.com>
2018-12-30 23:31:55 +0900

    update storybook

--- src/components/Card/Card.story.js
+++ src/components/Card/Card.story.js
@@ -1,13 +1,10 @@
 import { storiesOf } from '@storybook/vue'
-import VueInfoAddon from 'storybook-addon-vue-info'
 import Card from './Card.vue'
-import store from '~/src/store'
-import '~/src/assets/css/storybook.css'
+import store from '~src/store'
+import '~src/assets/css/storybook.css'
 
-storiesOf('Card', module)
-  .addDecorator(VueInfoAddon)
-  .add('default', () => ({
-    components: { Card },
-    store,
-    template: `<Card />`
-  }))
+storiesOf('Card', module).add('default', () => ({
+  components: { Card },
+  store,
+  template: `<Card />`
+}))
--- src/components/Navi/Navi.story.js
+++ src/components/Navi/Navi.story.js
@@ -1,13 +1,10 @@
 import { storiesOf } from '@storybook/vue'
-import VueInfoAddon from 'storybook-addon-vue-info'
 import Navi from './Navi.vue'
-import store from '~/src/store'
-import '~/src/assets/css/storybook.css'
+import store from '~src/store'
+import '~src/assets/css/storybook.css'
 
-storiesOf('Navi', module)
-  .addDecorator(VueInfoAddon)
-  .add('default', () => ({
-    components: { Navi },
-    store,
-    template: `<Navi />`
-  }))
+storiesOf('Navi', module).add('default', () => ({
+  components: { Navi },
+  store,
+  template: `<Navi />`
+}))
